339523872a8df3da772aa84240fc3874ed574638
YusupovUlugbek <ulugbekyusupov272@gmail.com>
2021-06-19 16:55:37 +0500

    master2 commit

--- app/src/main/java/uz/gita/gitdemo/MainActivity.kt
+++ app/src/main/java/uz/gita/gitdemo/MainActivity.kt
@@ -1,11 +1,13 @@
 package uz.gita.gitdemo
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        println("this is master second commit")
     }
 }
